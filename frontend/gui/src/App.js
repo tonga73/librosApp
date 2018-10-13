818b07d7883ca29194f880c2c20db68c26c43116
gastoire <buzzcutseason73@gmail.com>
2018-10-13 08:39:01 +0100

    React y Ant Design configurados (prueba)

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
 import 'antd/dist/antd.css';
 
 import CustomLayout from './containers/Layout';
+import ArticleList from './containers/ArticleListView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CustomLayout >
-
+        <CustomLayout>
+          <ArticleList />
         </CustomLayout>
       </div>
     );
